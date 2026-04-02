46c1bed6e1f9d024e422886ba5fead2087506dd1
July Lemos da Silva <julylemos1@hotmail.com>
2026-04-02 14:45:33 -0300

    feat: implementação da api e retrofit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -0,0 +1,30 @@
+package br.edu.ifsp.scl.sdm.currencyconverter.model.api
+
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.ConversionResult
+import br.edu.ifsp.scl.sdm.currencyconverter.model.domain.CurrencyList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface CurrencyConverterApiService {
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 0efca81af1mshee5312859e2d216p1d7e9ajsna283087c44d0"
+    )
+    @GET("list")
+    fun getCurrencies(): Call<CurrencyList>
+
+    @Headers(
+        "x-rapidapi-host: currency-converter5.p.rapidapi.com",
+        "x-rapidapi-key: 0efca81af1mshee5312859e2d216p1d7e9ajsna283087c44d0"
+    )
+    @GET("convert")
+    //com corroutinas, se for com fluxos, é o momento adequado de estabelecer quais serão os fluxos
+    fun convert(
+        @Query("from") from: String,
+        @Query("to") to: String,
+        @Query("amount") amount: String
+    ): Call<ConversionResult>
+}
